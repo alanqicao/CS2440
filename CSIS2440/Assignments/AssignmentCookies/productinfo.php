89e4b98d9c23138809f63682419e2ba9b68ef081
alanqicao <alanqicao@gmail.com>
2021-02-10 12:10:33 -0700

    CE04

--- CSIS2440/Assignments/AssignmentCookies/productinfo.php
+++ CSIS2440/Assignments/AssignmentCookies/productinfo.php
@@ -10,8 +10,92 @@ and open the template in the editor.
         <title>Check Us Out</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+                <!--            navBar-->
+            <header class="header__container">
+
+                <nav class = "primary-nav">
+
+                    <a href="index.html" class ="btn btn-alt">Home</a>
+                    <a href="productinfo.php" class ="btn btn-alt">Product Information</a>
+                    <a href="SignUp.html" class ="btn btn-alt">Sign up</a>
+                    <a href="login.php" class ="btn btn-alt">Login</a>
+                    <a href="order.html" class ="btn btn-alt">Order Online</a>
+
+
+                </nav>
+
+            </header>
+                
+        <div class="container-fluid">
+            <div class="row">
+                <form class="form-group" action="response.php" method="post">
+                    <fieldset>
+                        <legend><h3>Seclect a product or service</h3></legend>
+
+                        <!--ask user/ name-->
+                        <div class="form-group">
+                            <label class="col-md-8 control-label" for="UserName">User Name</label>  
+                            <div class="col-md-8">
+                                <input id="UserName" name="UserName" type="text" placeholder="User Name" required="required" class="form-control input-md" name="UserName">
+                                <span class="help-block">This is the name you will go by</span>  
+                            </div>
+                        </div>
+
+                        <!--select a product or service-->
+                        <div class="form-group">
+                            <label class="col-md-8 control-label" for="Products">Products and Service</label>  
+                            <div class="col-md-8">
+                               
+                                <select name="Products" id="Products">
+                                    <option value="0" >Cookies and Brownies</option>
+                                    <option value="1" >Breads and Pastries</option>
+                                    <option value="2" >Cakes and Pies</option>
+                                    <option value="3" >Birthday Cake</option>
+                                    <option value="4" >Party Box</option>
+                                </select>
+                                <span class="help-block">Check out our products</span>  
+                            </div>
+                        </div>
+                        
+                        <!--ask location or service-->
+                        
+                        
+                        <div class="form-group">
+                            <label class="col-md-8 control-label" for="ShipState">Ship State</label>  
+                            <div class="col-md-8">
+                                <input id="ShipState" name="ShipState" type="text" placeholder="UT" required="required" maxlength="2" class="form-control input-md" name="ShipState">
+                                <span class="help-block">This is the shipping state your will use, Only use abbreviations </span>  
+                            </div>
+                        </div>
+                        
+                        <!--submit-->
+                        
+                        
+                        <div class="form-group">
+ 
+                            <div class="col-md-8">
+                                <input id="Submit" name="Submit" type="submit"  class="form-control input-group-btn" name="Submit">
+                                 
+                            </div>
+                        </div>
+                        
+                        
+                        
+
+                    </fieldset>
+                </form>
+
+
+
+
+             
+
+
+            </div>
+
+        </div>
+
+
+
     </body>
 </html>
--- CSIS2440/Assignments/AssignmentCookies/response.php
+++ CSIS2440/Assignments/AssignmentCookies/response.php
@@ -4,14 +4,77 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+$UserName = htmlentities($_POST['UserName']);
+$UserName = strtolower($UserName);
+$UserName = ucwords($UserName);
+$Products = $_POST['Products'];
+$ShipState = htmlentities($_POST['ShipState']);
+$ShipState = strtoupper($ShipState);
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="css/response.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
-        // put your code here
+        //print out name and welcome message
+        print("<p class='title'>Wellcome $UserName, to Craft Cookie Cottage. A fun way to buy cookies online</p>");
+        
+        //show an image of the product or service they slect    and 
+        //show the product text from productinfo for the product
+  
+               $ProductArray = file('text/productinfo.txt');
+        switch($Products){
+            case 0:
+                print(" <img class='products__image' src='img/product1.jpg' alt=''> ");
+                print( "</br>".$Product = $ProductArray[0].$ProductArray[1].$ProductArray[2]);
+                break;
+            case 1:
+                print(" <img class='products__image' src='img/product2.jpg' alt=''> ");
+                print("</br>".$Product = $ProductArray[3].$ProductArray[4]);
+                break;
+            case 2:
+                print(" <img class='products__image' src='img/product3.jpg' alt=''> ");
+                print("</br>".$Product = $ProductArray[5].$ProductArray[6]);
+                break;
+            case 3:
+                print(" <img class='products__image' src='img/product4.jpg' alt=''> ");
+                print("</br>".$Product = $ProductArray[7].$ProductArray[8]);
+                break;
+             case 4:
+                 print(" <img class='products__image' src='img/product5.jpg' alt=''> ");
+                print("</br>".$Product = $ProductArray[9].$ProductArray[10]);
+                break;
+             default:
+                echo "Pleas make a new selection";
+        }
+        
+        //check if the asking location is in the listed service area
+        //read file
+         $AvailablState = explode(",",file_get_contents("text/servicearea.txt"));
+         
+         $check = False; 
+         
+        for($i=0; $i<count($AvailablState);$i++){
+
+            if($ShipState==$AvailablState[$i]){
+               $check = True;    
+            }     
+        }
+           
+           
+         if($check)
+         {
+             print("</br></br> Your state: ".$ShipState." are available for shipping.");
+         
+         }
+         else{
+             print("</br> </br> Your state: ".$ShipState." is not in our service area.");
+         }
+        
         ?>
     </body>
 </html>
